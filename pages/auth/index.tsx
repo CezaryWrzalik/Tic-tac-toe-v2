dfc4b9edc55909e36dbe733ced2d14e443ec8ac1
CezaryWrzalik <wrzalikc@gmail.com>
2022-04-11 18:33:00 +0200

    removed useRouter in auth index

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,16 +1,7 @@
-import { getSession, GetSessionParams, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { getSession, GetSessionParams} from "next-auth/react";
 import Auth from "../../components/auth/Sign";
 
-
-
 const AuthPage = () => {
-  const router = useRouter();
-  const { data: session } = useSession()
-  if(session){
-    console.log('heyyy');
-    router.push('./multiplayer')
-  }
   return (
     <>
       <Auth />;
